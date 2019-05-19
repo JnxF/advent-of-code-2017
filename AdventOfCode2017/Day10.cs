86d7693dfa1333d0e5ec5eb80435afc479b1d53e
JnxF <paco.7070@hotmail.com>
2019-05-19 17:08:31 +0200

    Day 10

---
+++ AdventOfCode2017/Day10.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2017
+{
+    public class Day10 : ISolver<int>
+    {
+        public readonly string input;
+        public readonly int[] list;
+
+        public Day10(string input, int listSize)
+        {
+            this.input = input;
+            list = Enumerable.Range(0, listSize).ToArray();
+        }
+
+        public Day10()
+        {
+            input = "94,84,0,79,2,27,81,1,123,93,218,23,103,255,254,243";
+            list = Enumerable.Range(0, 256).ToArray();
+        }
+
+        int[] Input()
+        {
+            return input.Split(",")
+                .Select(i => int.Parse(i))
+                .ToArray();
+        }
+
+
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void ReverseSubarray<T>(T[] arr, int i, int j)
+        {
+            int n = arr.Length;
+            // Normal reverse
+            if (i < j)
+            {
+                for (int t = 0; t <= (j-i)/2; ++t)
+                {
+                    Swap(ref arr[i + t], ref arr[j - t]);
+                }
+            }
+            else
+            {
+                while (i != j)
+                {
+                    Swap(ref arr[i], ref arr[j]);
+                    i = (i + 1) % n;
+                    --j;
+                    if (i == 0 && j == -1) break;
+                    if (j < 0)
+                    {
+                        j = n - 1;
+                    }
+                }
+            }
+            
+        }
+        
+        public int FirstPart()
+        {
+            int currentPosition = 0;
+            int skipSize = 0;
+            int n = list.Length;
+            foreach (var length in Input())
+            {
+                int endReverse;
+                if (length == 0)
+                    endReverse = currentPosition;
+                else
+                    endReverse = (currentPosition + length - 1) % n;
+                ReverseSubarray(list, currentPosition, endReverse);
+                currentPosition += length + skipSize;
+                currentPosition %= n;
+                ++skipSize;
+            }
+            return list[0] * list[1];
+        }
+
+        public int SecondPart()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode2017Tests/Day10Tests.cs
@@ -0,0 +1,43 @@
+﻿using AdventOfCode2017;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AdventOfCode2017Tests
+{
+    public class Day10Tests
+    {
+        
+
+        [Fact]
+        public void Day10_Swap()
+        {
+            var a = 10;
+            var b = 20;
+            AdventOfCode2017.Day10.Swap(ref a, ref b);
+            Assert.Equal(20, a);
+            Assert.Equal(10, b);
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3 }, 0, 2, new[] { 3, 2, 1 })]
+        [InlineData(new[] { 10, 20, 30, 40, 50 }, 0, 1, new[] { 20, 10, 30, 40, 50 })]
+        [InlineData(new[] { 10, 20, 30, 40, 50 }, 4, 4, new[] { 10, 20, 30, 40, 50 })]
+        [InlineData(new[] { 1, 2, 3, 4, 5}, 1, 3, new[] { 1, 4, 3, 2, 5 })]
+        [InlineData(new[] { 1, 2, 3, 4, 5}, 3, 1, new[] { 5, 4, 3, 2, 1 })]
+        [InlineData(new[] { 1, 2, 3, 4, 5}, 4, 0, new[] { 5, 2, 3, 4, 1 })]
+        public void Day10_ReverseSubarray(int[] actual, int i, int j, int[] expected)
+        {
+            AdventOfCode2017.Day10.ReverseSubarray(actual, i, j);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Day10_FirstPart()
+        {
+            var day = new Day10("3, 4, 1, 5", 5);
+            Assert.Equal(12, day.FirstPart());
+        }
+    }
+}
